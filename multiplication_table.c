7e5b365e4dd9bb2c237cb25de48ba8ec3705ec03
vaidyaaashutosh06-cell <vaidyaaashutosh06@gmail.com>
2026-05-28 21:21:49 +0530

    Multiplication_table

---
+++ multiplication_table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number whose multiplication table is to be printed\n");
+    scanf("%d",&num);
+    printf("The multiplication table of %d is\n",num);
+    for(int i=0;i<10;i++)
+    {
+        printf("%d*%d=%d\n",num,i+1,(i+1)*num);
+
+    }
+    return 0;
+}
