ab1c126b87ea6c1872ccf52e000e502b4db6db9f
vaidyaaashutosh06-cell <vaidyaaashutosh06@gmail.com>
2026-05-31 11:09:25 +0530

    Day_4_Again

---
+++ fibonacci_Nth_series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n, a = 0, b = 1, c, i;
+    printf("Enter n:");
+    scanf("%d", &n);
+    if (n == 0)
+    {
+        printf("Nth fibonacci number=%d", a);
+    }
+    else if (n == 1)
+    {
+        printf("Nth fibonacci number=%d", b);
+    }
+    else
+    {
+        for (i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("Nth fibonacci number=%d", b);
+    }
+    return 0;
+}
